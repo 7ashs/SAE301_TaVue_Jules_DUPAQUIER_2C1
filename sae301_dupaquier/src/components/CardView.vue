cdb5eb0553ead8d4847942ca27321c0045c451d8
7ashs <dupaquier.jules@gmail.com>
2023-10-25 01:31:14 +0200

    Card.vue Rework

--- sae301_dupaquier/src/components/CardView.vue
+++ sae301_dupaquier/src/components/CardView.vue
@@ -2,8 +2,8 @@
 <div class="flex space-x-4">
     <!-- Première carte -->
     <div class="relative w-64 h-64 bg-bleuclair rounded-lg p-4">
-        <!-- Image au centre -->
-        <img src="/images/lunettes.svg" alt="Lunettes" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-auto">
+        <!-- Image centrée dans le cadre -->
+        <img src="/images/lunettes.svg" alt="Lunettes" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 h-auto">
         <!-- Bandeau bleu en bas -->
         <div class="absolute bottom-0 left-0 w-full h-16 bg-bleu flex items-center justify-between px-4">
             <span class="text-blanc font-michroma uppercase">Classique</span>
@@ -13,11 +13,7 @@
             </div>
         </div>
     </div>
-
 </div>
-
-
-
 
 </template>
     
