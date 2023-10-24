6abc6711c1250adcdc5a9d7a349cc357cc918841
7ashs <dupaquier.jules@gmail.com>
2023-10-24 14:38:51 +0200

    Import Fonts

--- sae301_dupaquier/src/App.vue
+++ sae301_dupaquier/src/App.vue
@@ -1,24 +1,10 @@
 <script setup>
+import HeaderView from './components/HeaderView.vue';
+
 </script>
 
 <template>
-    <header class="bg-black h-[130px]">
-        <RouterLink class="text-black" to="/accueil">Accueil</RouterLink>
-    </header>
-
-    <header class="bg-gray-200 py-2 md:py-4">
-    <nav class="container px-4 mx-auto md:flex md:items-center">
-      <div class="flex justify-between items-center">
-        <RouterLink class="text-black" to="/accueil">Accueil</RouterLink>
-        <a href="#" class="font-bold text-xl text-gray-800">Mon site</a>
-        <div class="flex items-center">
-          <a href="#" class="text-gray-600">À propos</a>
-          <a href="#" class="text-gray-600">Contact</a>
-        </div>
-      </div>
-    </nav>
-  </header>
-    <RouterView />
+    <HeaderView />
 </template>
 
 <style scoped>
---
+++ sae301_dupaquier/src/components/HeaderView.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="bg-gray-900 text-white">
+    <div class="container mx-auto p-4 flex items-center justify-between">
+      <div class="flex items-center">
+        <img src="/images/logo.svg" alt="TaVue-Logo" class="h-10 w-10" />
+        <nav class="hidden md:flex space-x-4 ml-4">
+          <router-link to="/">Accueil</router-link>
+          <router-link to="/catalogue">Catalogue</router-link>
+          <router-link to="/personnaliser">Personnaliser</router-link>
+          <router-link to="/agences">Nos agences</router-link>
+          <router-link to="/contact">Contactez-nous</router-link>
+          <!-- Ajoutez d'autres liens ici -->
+        </nav>
+      </div>
+      <div class="flex items-center space-x-4">
+        <router-link to="/compte">
+          <i class="fas fa-user"></i>
+        </router-link>
+        <router-link to="/panier">
+          <i class="fas fa-shopping-cart"></i>
+        </router-link>
+        <button @click="toggleMobileMenu" class="md:hidden">
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+    </div>
+    <!-- Menu hamburger pour mobile -->
+    <div v-if="mobileMenuOpen" class="bg-gray-900">
+      <nav class="flex flex-col items-center space-y-2 p-4">
+        <router-link to="/">Accueil</router-link>
+        <router-link to="/catalogue">Catalogue</router-link>
+        <router-link to="/personnaliser">Personnaliser</router-link>
+        <router-link to="/agences">Nos agences</router-link>
+        <router-link to="/contact">Contactez-nous</router-link>
+        <!-- Ajoutez d'autres liens ici -->
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobileMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMobileMenu() {
+      this.mobileMenuOpen = !this.mobileMenuOpen;
+    },
+  },
+};
+</script>
