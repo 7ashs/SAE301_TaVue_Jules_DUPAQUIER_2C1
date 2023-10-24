7788474155dbac59e0033886ebea166e2434ec07
7ashs <dupaquier.jules@gmail.com>
2023-10-24 16:29:11 +0200

    Footer 1

---
+++ sae301_dupaquier/src/components/FooterView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-noir py-1 font-inter">
+    <div class="bg-black py-2 text-center text-blanc mt-1 mb-1">
+      <p>Notre marque est 100% française !</p>
+    </div>
+    <div class="flex items-center justify-center">
+      <div class="bg-bleu w-full h-1"></div>
+      <div class="bg-blanc w-full h-1"></div>
+      <div class="bg-rouge w-full h-1"></div>
+    </div>
+  </div>
+    <div class="bg-noir20 text-left py-4">
+      <img src="/images/logo-text.svg" alt="Logo-Text" class="h-32 w-32 ml-5" />
+
+      <div class="flex flex-col ml-10 mt-10 space-y-2 font-inter">
+        <router-link to="/conditions-generales" class="text-blanc hover:text-bleu">CGV / CGU</router-link>
+        <router-link to="/politique-confidentialite" class="text-blanc hover:text-bleu">Politique de Confidentialité</router-link>
+        <router-link to="/faq" class="text-blanc hover:text-bleu">Foire aux Questions (FAQ)</router-link>
+        <router-link to="/a-propos" class="text-blanc hover:text-bleu">À propos de nous</router-link>
+        <router-link to="/declarations-legales" class="text-blanc hover:text-bleu">Déclarations Légales</router-link>
+      </div>
+      <div class="border-t border-blanc py-2 mt-5 mb-5 ml-10 mr-10"></div>
+      <p class="text-blanc font-inter text-center">&copy;2023 TAVUE - TOUS DROITS RÉSERVÉS.</p>
+      <div class="flex justify-center space-x-4 pt-4">
+        <a href="/404" class="text-blanc hover:text-bleu">
+          <img src="/images/facebook-icon.svg" alt="Facebook" class="h-6 w-6" />
+        </a>
+        <a href="/404" class="text-blanc hover:text-bleu">
+          <img src="/images/twitter-icon.svg" alt="Twitter" class="h-6 w-6" />
+        </a>
+        <a href="/404" class="text-blanc hover:text-bleu">
+          <img src="/images/youtube-icon.svg" alt="YouTube" class="h-6 w-6" />
+        </a>
+        <a href="/404" class="text-blanc hover:text-bleu">
+          <img src="/images/instagram-icon.svg" alt="Instagram" class="h-6 w-6" />
+        </a>
+      </div>
+    </div>
+</template>
+  
+<script>
+ 
+</script>
+  
+<style>
+
+</style>
+  
